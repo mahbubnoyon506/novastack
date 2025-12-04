f5b92d2f93c45ed11669dba4c6e76298fae4baee
mahbubnoyon506 <mahbubnoyon506@gmail.com>
2025-12-04 20:39:27 +0600

    Services section created

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import { Button } from "@/components/ui/button";
 import Hero from "./components/Hero";
+import Services from "./components/Services";
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <Services />
     </>
   );
 }
